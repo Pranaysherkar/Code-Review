8b5ea5d0507a5f64e8d5ab98976d56f0f215123c
Pranay Sherkar <116248469+Pranaysherkar@users.noreply.github.com>
2025-04-17 16:06:44 +0530

    vercel.json file setup

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -4,12 +4,15 @@ const cors = require('cors');
 const app = express();
 
 app.use(cors())
-app.use(express.json()); 
-app.use(express.urlencoded({ extended: true })); 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.get('/', (req, res) => {
-    res.send('Hello World!');
+    res.send({
+        activeStatus: true,
+        error: false
+    });
 });
 
-app.use('/ai',aiRoutes);
+app.use('/ai', aiRoutes);
 
 module.exports = app;
